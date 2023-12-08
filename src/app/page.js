5fef604819ac790467d2255b4abd4ba4765aa842
ValentinaKVB <rdirf@yandex.ru>
2023-12-08 17:03:01 +0300

    До изменения next.config.js

--- src/app/page.js
+++ src/app/page.js
@@ -1,95 +1,139 @@
-import Image from 'next/image'
+"use client"
+
+import { useRef } from 'react';
 import styles from './page.module.css'
 
 export default function Home() {
+
+  function load(url) {
+    return new Promise(function (resolve, reject) {
+      const request = new XMLHttpRequest();
+      request.onreadystatechange = function () {
+        if (this.readyState === 4 && this.status == 200) {
+          resolve(this.response);
+        } else {
+          reject(this.status);
+        }
+      };
+      request.open('GET', url, true);
+      request.send();
+    });
+  }
+
+  const url = '/aldis/promiseTest';
+  const msgRef = useRef(null);
+
+  function handleClick() {
+    load(url)
+    .then((response) => {
+      const result = JSON.parse(response);
+      msgRef.current.textContent = result.message;
+    })
+    .catch((error) => {
+      msgRef.current.textContent = `Error getting the message, HTTP status: ${error}`;
+    });
+  }
+
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>src/app/page.js</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
+    <div className={styles.content}>
+      <div className={styles.container}>
+        <div className={styles.message} ref={msgRef}></div>
+        <button className={styles.btnGet} onClick={handleClick}>Get Message</button>
       </div>
-
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore starter templates for Next.js.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+    </div>
   )
 }
+
+
+// export default function Home() {
+//   return (
+//     <main className={styles.main}>
+//       <div className={styles.description}>
+//         <p>
+//           Get started by editing&nbsp;
+//           <code className={styles.code}>src/app/page.js</code>
+//         </p>
+//         <div>
+//           <a
+//             href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
+//             target="_blank"
+//             rel="noopener noreferrer"
+//           >
+//             By{' '}
+//             <Image
+//               src="/vercel.svg"
+//               alt="Vercel Logo"
+//               className={styles.vercelLogo}
+//               width={100}
+//               height={24}
+//               priority
+//             />
+//           </a>
+//         </div>
+//       </div>
+
+//       <div className={styles.center}>
+//         <Image
+//           className={styles.logo}
+//           src="/next.svg"
+//           alt="Next.js Logo"
+//           width={180}
+//           height={37}
+//           priority
+//         />
+//       </div>
+
+//       <div className={styles.grid}>
+//         <a
+//           href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
+//           className={styles.card}
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           <h2>
+//             Docs <span>-&gt;</span>
+//           </h2>
+//           <p>Find in-depth information about Next.js features and API.</p>
+//         </a>
+
+//         <a
+//           href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
+//           className={styles.card}
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           <h2>
+//             Learn <span>-&gt;</span>
+//           </h2>
+//           <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
+//         </a>
+
+//         <a
+//           href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
+//           className={styles.card}
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           <h2>
+//             Templates <span>-&gt;</span>
+//           </h2>
+//           <p>Explore starter templates for Next.js.</p>
+//         </a>
+
+//         <a
+//           href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
+//           className={styles.card}
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           <h2>
+//             Deploy <span>-&gt;</span>
+//           </h2>
+//           <p>
+//             Instantly deploy your Next.js site to a shareable URL with Vercel.
+//           </p>
+//         </a>
+//       </div>
+//     </main>
+//   )
+// }
